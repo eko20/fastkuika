a5f5d7c250eadfeb2fa4a27ebf7163663cf01b71
eko20 <109029696+eko20@users.noreply.github.com>
2024-10-19 16:13:13 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_openai import ChatOpenAI
+from typing import Union
 
 app = FastAPI()
 
 # Define a request body model using Pydantic
 class QueryRequest(BaseModel):
     query: str
+    
+
+
+    
+file_path = "C:\\Users\\mekin\\Desktop\\IPCC_AR6_WGII_Chapter03.pdf"
+
+
 
 @app.post("/get-query")
 async def get_query(request: QueryRequest):
     # Access the query from the request body
-    return {"received_query": request.query}
+    return {"received_query": request.query,
+            "lessonplan": "calişcan",
+            "çabaliyican": "popkek severeim"
+            }
